0352709a47ff15b579225bdd7ae89cf74dfa7e57
2216851 <152493548+2216851@users.noreply.github.com>
2024-01-18 01:08:57 +0000

    Added user inputted year

--- F1.py
+++ F1.py
@@ -1,13 +1,13 @@
 import requests
 
+year = input("Enter the season year: ")
 
-drivers = 'https://ergast.com/api/f1/2023/driverStandings.json'
+drivers = f'https://ergast.com/api/f1/{year}/driverStandings.json' #adds user inputted year to URL
 
-r = requests.get(drivers)
+r = requests.get(drivers)   #send GET request to API URL defined above as 'drivers'
 
 data = r.json()
 
 print(data)
-
 
 
