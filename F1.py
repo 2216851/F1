edcd8efd2f234e7470190dc64e7cdb4326df299f
2216851 <152493548+2216851@users.noreply.github.com>
2024-01-18 22:00:00 +0000

    Set default value for driver code as older years are missing data, was crashing code. Also added option for user to exit from terminal.

--- F1.py
+++ F1.py
@@ -1,67 +1,74 @@
 import requests
 championship = '' #setting so below loop while requires break via correct user input
+while True:
+    year = input("Enter the season year: ") #user input stored as year
 
-year = input("Enter the season year: ") #user input stored as year
-
-#while loop to ensure user enters valid input
-while championship != '1' or championship != '2':       
-    championship = input(f"Enter 1 for {year} drivers standings, or 2 for constructors standings: ")
-    if championship == '1' or championship == '2':
+    #sets year to lowercase to accept any case input from user. break ends program.
+    if year.lower() == 'exit':
+        print("Exiting program...")
         break
-
-if championship == '1': 
-
-    drivers = f'https://ergast.com/api/f1/{year}/driverStandings.json' #adds user inputted year to URL
-    rd = requests.get(drivers)  #send GET request to API
-
-    if rd.status_code == 200: #checks status code success, if error prints status code
-        data = rd.json()  #store json data from request in data
-
-        print(f"Retrieving {year} drivers championship standings...")
-
-        #extract data from json structure to dStandings, 0 index is for entire championship not rd1
-        dStandings = data['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings']
-        #loop through extracting data for each driver  
-        for driverData in dStandings:       
-            position = driverData['position']
-            givenName = driverData['Driver']['givenName'] 
-            familyName = driverData['Driver']['familyName']
-            code = driverData['Driver']['code']
-            points = driverData['points']
-            wins = driverData['wins']
-            team = driverData['Constructors'][0]['name'] #access constructors list
         
-            #output data into cli, each driver seperated by empty line
-            print(f"{position}. {code}. {givenName} {familyName}: {points} pts, {wins} wins. ")           
-            print(f"{team}")
-            print()
+    championship = '' #setting so below loop while requires break via correct user input
+
+    #while loop to ensure user enters valid input
+    while championship != '1' or championship != '2':       
+        championship = input(f"Enter 1 for {year} drivers standings, or 2 for constructors standings: ")
+        if championship == '1' or championship == '2':
+            break
+
+    if championship == '1': 
+
+        drivers = f'https://ergast.com/api/f1/{year}/driverStandings.json' #adds user inputted year to URL
+        rd = requests.get(drivers)  #send GET request to API
+
+        if rd.status_code == 200: #checks status code success, if error prints status code
+            data = rd.json()  #store json data from request in data
+
+            print(f"Retrieving {year} drivers championship standings...")
+
+            #extract data from json structure to dStandings, 0 index is for entire championship not rd1
+            dStandings = data['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings']
+            #loop through extracting data for each driver  
+            for driverData in dStandings:       
+                position = driverData['position']
+                givenName = driverData['Driver']['givenName'] 
+                familyName = driverData['Driver']['familyName']
+                code = driverData['Driver'].get('code', '---') #set default value where data is missing
+                points = driverData['points']
+                wins = driverData['wins']
+                team = driverData['Constructors'][0]['name'] #access constructors list
+            
+                #output data into cli, each driver seperated by empty line
+                print(f"{position}. {code}. {givenName} {familyName}: {points} pts, {wins} wins. ")           
+                print(f"{team}")
+                print()
+
+        else:
+            #print status code if not 200 OK
+            print(f"ERROR {rd.status_code}: Unable to retrieve data.")
 
     else:
-        #print status code if not 200 OK
-        print(f"ERROR {rd.status_code}: Unable to retrieve data.")
+        #if user enters 2, GET request for constructors standings
+        constructors = f'https://ergast.com/api/f1/{year}/constructorStandings.json'
+        rc =requests.get(constructors)
 
-else:
-    #if user enters 2, GET request for constructors standings
-    constructors = f'https://ergast.com/api/f1/{year}/constructorStandings.json'
-    rc =requests.get(constructors)
+        if rc.status_code == 200:
+            data =rc.json()
 
-    if rc.status_code == 200:
-        data =rc.json()
+            print(f"Retrieving {year} constructors standings...")
 
-        print(f"Retrieving {year} constructors standings...")
+            #extract data to cStandings, loop through each constructor and extract data
+            cStandings = data['MRData']['StandingsTable']['StandingsLists'][0]['ConstructorStandings']
+            for constructorData in cStandings:
+                position = constructorData['position']
+                name = constructorData['Constructor']['name']
+                points = constructorData['points']
+                wins = constructorData['wins']
 
-        #extract data to cStandings, loop through each constructor and extract data
-        cStandings = data['MRData']['StandingsTable']['StandingsLists'][0]['ConstructorStandings']
-        for constructorData in cStandings:
-            position = constructorData['position']
-            name = constructorData['Constructor']['name']
-            points = constructorData['points']
-            wins = constructorData['wins']
+                #output data to terminal, each constructor seperated by empty line
+                print(f'{position}. {name} - {points} pts, {wins} wins.')
+                print()
 
-            #output data to terminal, each constructor seperated by empty line
-            print(f'{position}. {name} - {points} pts, {wins} wins.')
-            print()
-
-    else:
-        #if status not 200 OK, print status code
-        print(f"ERROR {rc.status_code}: Unable to retrieve data.")
+        else:
+            #if status not 200 OK, print status code
+            print(f"ERROR {rc.status_code}: Unable to retrieve data.")
