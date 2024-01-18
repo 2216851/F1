0f1607420c94d0ef59aa54376da37a25118cc249
2216851 <152493548+2216851@users.noreply.github.com>
2024-01-18 13:18:31 +0000

    loop added to retrieve and print only desired information, tidies up output

--- F1.py
+++ F1.py
@@ -3,13 +3,23 @@ import requests
 year = input("Enter the season year: ")
 
 drivers = f'https://ergast.com/api/f1/{year}/driverStandings.json' #adds user inputted year to URL
-
 r = requests.get(drivers)   #send GET request to API URL defined above as 'drivers'
 
-if r.status_code == 200:
-
+if r.status_code == 200:       #checks status code success, if error prints status code
     data = r.json()
-    print(data)
+
+    dStandings = data['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'] #extracting data, index set to 0 for whole season 
+    for driverData in dStandings:       #loop through each driver record storing below variables
+        position = driverData['position']
+        givenName = driverData['Driver']['givenName'] 
+        familyName = driverData['Driver']['familyName']
+        code = driverData['Driver']['code']
+        points = driverData['points']
+        
+        print(f"Pos: {position}")           #prints information for each driver with an empty line sperating them
+        print(f"Driver: {givenName} {familyName} {code}")
+        print(f"Points: {points}")
+        print()
 
 else:
     print(f"ERROR {r.status_code}: Unable to retrieve data.")
